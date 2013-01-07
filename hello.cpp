9949ecbfcaa092640442ecb245a2df0a0c0b673d
david <davidguan@realtek.com>
2013-01-07 19:11:51 +0800

    commit C
      Add hello function

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+
+using namespace std; 
+
+void show_main(void){
+   cout << "show main" << endl;
+}
+
+int main() { 
+
+ cout << "Hello! Git\n"; 
+
+ show_main();
+
+ return 0; 
+}
